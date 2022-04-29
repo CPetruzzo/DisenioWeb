3292d7ae73bf2e6ac938a607e798ee06c14b9868
Facundo <facu03@gmail.com>
2022-04-29 18:21:10 -0300

    angular

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AboutMeComponent } from './componentes/about-me/about-me.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    EducacionComponent,
+    PieDePaginaComponent,
+    ContactoComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
