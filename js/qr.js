dd51aca3d6100690f1e9093ddb93da227fd20d58
Facundo <facu03@gmail.com>
2022-10-25 11:51:50 -0300

    butonstyled

--- js/qr.js
+++ js/qr.js
@@ -5,6 +5,7 @@ const formulario = document.getElementById('formulario');
 const QR = new QRCode(contenedorQR);
 
 formulario.addEventListener('submit', (e)=>{
+  
   e.preventDefault();
   QR.makeCode(formulario.link.value); 
 });
