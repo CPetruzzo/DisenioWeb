80325f2bf17dc9fcbed053b2195890244027a994
Facundo <facu03@gmail.com>
2022-10-31 11:20:01 -0300

    renegandoConDBJSON

--- js/practicasJS.js
+++ js/practicasJS.js
@@ -1,4 +1,8 @@
 // document.write("hola mundo");
 
-let h1 = document.write("hola mundo");
+// let h1 = document.write("hola mundo");
 
+    
+let h2 = document.write(new Date().getFullYear());
+
+let h3 = document.write(new Date().getMonth());
