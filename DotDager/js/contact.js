edc352eb70e9365d67f25c72d175410c98b4db98
Facundo W <99218221+CPetruzzo@users.noreply.github.com>
2024-11-25 15:22:19 -0300

    feat(animations): :sparkles: adds fade animations and image

--- DotDager/js/contact.js
+++ DotDager/js/contact.js
@@ -1,11 +1,12 @@
     // Seleccionar el botón y la sección
     const toggleButton = document.getElementById('toggleButton');
     const socialNetworks = document.getElementById('socialNetworks');
+    const toggleImg = document.getElementById('toggleImg');
 
     // Agregar un evento de clic
     toggleButton.addEventListener('click', () => {
         const isActive = socialNetworks.classList.toggle('active');
         toggleButton.textContent = isActive ? "Ocultar" : "¡Contáctate conmigo!";
         toggleButton.setAttribute('aria-expanded', isActive);
-
+        toggleButton.style.display = isActive ? "none" : "none";
     });
